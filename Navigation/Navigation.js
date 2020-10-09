0cca20541a5aa71c4abeb6bb72070b90a5162936
Tatevik <tatevik@Dalims-iMac-2.local>
2020-10-09 11:51:31 +0200

    ReduxStoreFavoriteList

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,18 +1,28 @@
 import { createStackNavigator } from 'react-navigation-stack';
-import {createAppContainer } from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
+//import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Search from '../Components/Search'
 import FilmDetail from '../Components/FilmDetail'
 
 const SearchStackNavigator = createStackNavigator({
-  Search: {
-    screen: Search,
-    navigationOptions: {
-      title: 'Rechercher'
+    Search: {
+        screen: Search,
+        navigationOptions: {
+            title: 'Rechercher'
+        }
+    },
+    FilmDetail: { // Encore une fois j'ai mis le même nom que celui du component mais libre à vous de choisir un nom différent
+        screen: FilmDetail
     }
-  },
-  FilmDetail: { // Encore une fois j'ai mis le même nom que celui du component mais libre à vous de choisir un nom différent
-    screen: FilmDetail
-  }
 })
+
+/* const MoviesTabNavigator = createBottomTabNavigator({
+    Search: {
+      screen: SearchStackNavigator
+    },
+     Favorites: {
+      screen: Favorites
+    } 
+  }) */
 
 export default createAppContainer(SearchStackNavigator)
